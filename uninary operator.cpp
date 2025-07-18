687f81405d769c50a7d47ba7008ce58e5f87bc5b
Devisaimuggalla123 <muggalladevisai@gmail.com>
2025-07-18 15:28:02 +0530

    unary operator

---
+++ uninary operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int x=6;
+	cout<< "original value" <<x<<endl;
+	x++;
+	cout<<"after x++"<<x<<endl;
+	++x;
+	cout<<"after ++x"<<x<<endl;
+	x--;
+	cout<<"after x--"<<x<<endl;
+	--x;
+	cout<<"after x--"<<x<<endl;
+}
