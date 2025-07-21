e9ca686eb7324db280e6ec5e9a226c4effdfd2bb
Devisaimuggalla123 <muggalladevisai@gmail.com>
2025-07-21 14:55:19 +0530

    rw array using macross

---
+++ rw array using macrosss.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#define n 3
+
+int main()
+{
+    int arr[n],i;
+  for(i=0;i<n;i++)
+  {
+      cout<<"Enter element";
+      cin>>arr[i];
+  }
+    for(i=0;i<n;i++)
+  {
+      cout<<arr[i]<<endl;
+      
+  }
+}
+
