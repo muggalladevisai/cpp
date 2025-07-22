d046571e0543cd7b8cd2c68b018d5da25bdae65f
Devisaimuggalla123 <muggalladevisai@gmail.com>
2025-07-22 14:23:22 +0530

    readingandwriting of array element

---
+++ readingandwriting of array element.cpp
@@ -0,0 +1,18 @@
+//READING  AND WRITING OF ELEMENTS
+#include <iostream>
+using namespace std;
+int main()
+{
+    int arr[3],i;
+  for(i=0;i<3;i++)
+  {
+      cout<<"Enter element";
+      cin>>arr[i];
+  }
+    for(i=0;i<3;i++)
+  {
+      cout<<arr[i]<<endl;
+  }
+  
+}
+
